aae9f5c8dc1f1cf3d82f131f0c59f8b3b96f39fb
mohsen <mirmohsenmosavi1367@gmail.com>
2021-05-08 18:40:07 +0430

    swagger is compelete

--- user/views/user.py
+++ user/views/user.py
@@ -17,11 +17,13 @@ from rest_framework.generics import GenericAPIView
 
 class EvaluationExpert(GenericAPIView):
     serializer_class = Evaluation_serializer
+    queryset = evModels.objects.all()
     def get(self, request):
         quaryset = evModels.objects.all()
         output = Evaluation_serializer(quaryset, many=True)
         return Response(output.data)
 
+# THE DATA MUST POST TO IT IS email, gender, username, name, password
     def post(self, request):
         data = JSONParser().parse(request)
         try:
@@ -37,23 +39,21 @@ class EvaluationExpert(GenericAPIView):
 
             u.set_password(data['password'])
             u.save()
-
-
             e = evModels.objects.create(user=u)
             s = Evaluation_serializer(e)
             return Response(s.data, status=status.HTTP_200_OK)
 
 
-class InsurserView(APIView):
-    serializer_class = Insurser_serializer
+class InsurserView(GenericAPIView):
 
-    ''' THE DATA MUST POST TO IT IS email, gender, username, name,
-    # password , insurer_start and insurer_end'''
+    serializer_class = Insurser_serializer
+    queryset = Insurer.objects.all()
 
     def get(self, requet):
         queryset = Insurer.objects.all()
         s = Insurser_serializer(queryset, many=True)
         return Response(s.data, status=status.HTTP_200_OK)
+# THE DATA MUST POST TO IT IS email, gender, username, name, password, insurer_start and insurer_end
 
     def post(self, request):
         data = JSONParser().parse(request)
@@ -80,11 +80,14 @@ class InsurserView(APIView):
 
 class CompaniView(GenericAPIView):
     serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+
     def get(self, request):
         queryset = Company.objects.all()
         s = CompanySerializer(queryset, many=True)
         return Response(s.data, status=status.HTTP_200_OK)
 
+# THE DATA MUST POST TO IT IS name, address, phone_number
     def post(self, request):
         data = JSONParser().parse(request)
         c = Company.objects.create(name=data['name'], address=data['address']
@@ -95,15 +98,16 @@ class CompaniView(GenericAPIView):
 
 class CompanyUSerView(GenericAPIView):
     serializer_class = UserCompanySerializer
+    queryset = Evalution.objects.all()
     def get(self, request):
         queryset = Evalution.objects.all()
         s = UserCompanySerializer(queryset, many=True)
         return Response(s.data, status=status.HTTP_200_OK)
 
-
+# THE DATA MUST POST TO IT IS companyname, location,  evalution_time, email
     def post(self, request):
         data = JSONParser().parse(request)
-        c = Company.objects.get(name=data['companyname'])
+        c = Company.objects.get(name=data['name'])
         E = Evalution.objects.create(location=data['location'], evalution_time=data['evalution_time'],
                                      user=CUser.objects.get(email=data['email']), company=c)
         s = UserCompanySerializer(E)
@@ -112,12 +116,15 @@ class CompanyUSerView(GenericAPIView):
 
 class PermissionView(GenericAPIView):
     serializer_class = PermissionSerialzer
+    queryset = Permission.objects.all()
+
     @check_permission("list_permission")
     def get(self, request):
         queryset = Permission.objects.all()
         s = PermissionSerialzer(queryset, many=True)
         return Response(s.data, status=status.HTTP_200_OK)
 
+# THE DATA MUST POST TO IT IS name, parent
     @check_permission("create_permission")
     def post(self, request):
         data = JSONParser().parse(request)
@@ -139,11 +146,13 @@ class PermissionView(GenericAPIView):
 
 class UserPermissionView(GenericAPIView):
     serializer_class = UserPermissionSerializer
+    queryset = UserPermission.objects.all()
     def get(self, request):
         queryset = UserPermission.objects.all()
         s = UserPermissionSerializer(queryset, many=True)
         return Response(s.data, status=status.HTTP_200_OK)
 
+# THE DATA MUST POST TO IT IS username, name
     def post(self, request):
         data = JSONParser().parse(request)
         check = UserPermission.objects.filter(user__username=data['username'], permission__name=data['name']).first()
